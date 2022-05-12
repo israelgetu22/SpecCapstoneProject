eefdc4eee6fb51f753e197c9c96075525462f098
Isarel Getu <israelgetu101@gmail.com>
2022-05-12 18:17:04 -0500

    Nice

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -3,7 +3,9 @@ import styled from "styled-components";
 import { cataegories } from "../data";
 import CategoryItem from "./CategoryItem";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+`;
 
 const Categories = () => {
   return (
