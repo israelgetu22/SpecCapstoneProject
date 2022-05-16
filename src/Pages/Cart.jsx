42619ef89f9bed0bd24c3e57aea0808782a68f4a
Isarel Getu <israelgetu101@gmail.com>
2022-05-15 23:58:26 -0500

    Responsive

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Announcement from "../Components/Announcement";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import { Add, Remove } from "@material-ui/icons";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
@@ -43,14 +44,79 @@ const Summary = styled.div`
   flex: 1;
 `;
 
-const ProductDetail = styled.div``;
-const Image = styled.div``;
-const Details = styled.div``;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
 const ProductName = styled.div``;
-const PriceDetail = styled.div``;
 const ProductId = styled.div``;
-const ProductColor = styled.div``;
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
 const ProductSize = styled.div``;
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+//const Summary = styled.div``;
+const SummaryTitle = styled.h1`
+  font-wight: 200;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: orange;
+  color: black;
+  font-weight: 600;
+`;
 
 const Cart = () => {
   return (
@@ -69,24 +135,78 @@ const Cart = () => {
         </Top>
         <Bottom>
           <Info>
-            <ProductDetail>
-              <Image src="" />
-              <Details>
-                <ProductName>
-                  <b>Product:</b> JESSIE THUNDER SHOES
-                </ProductName>
-                <ProductId>
-                  <b>ID:</b> JESSIE THUNDER SHOES
-                </ProductId>
-                <ProductColor />
-                <ProductSize>
-                  <b>Size</b>37.5
-                </ProductSize>
-              </Details>
-            </ProductDetail>
-            <PriceDetail>Price</PriceDetail>
+            <Product>
+              <ProductDetail>
+                <Image src="https://st2.depositphotos.com/4307429/7858/i/950/depositphotos_78581272-stock-photo-black-sneakers-on-white-background.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOES
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> JESSIE THUNDER SHOES
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size</b>37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount> 2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://i.pinimg.com/originals/1e/55/bd/1e55bdc1278d253fd677a8e2b5aa169c.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> AIR FORCE SHOES
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> JESSIE THUNDER SHOES
+                  </ProductId>
+                  <ProductColor color="gray" />
+                  <ProductSize>
+                    <b>Size</b>30.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount> 5</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 50</ProductPrice>
+              </PriceDetail>
+            </Product>
           </Info>
-          <Summary>Summary</Summary>
+          <Summary>
+            <SummaryTitle> ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice> $ 80 </SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice> $ 5.90 </SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount </SummaryItemText>
+              <SummaryItemPrice> Free </SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice> $ 80 </SummaryItemPrice>
+            </SummaryItem>
+            <Button> CHECKOUT NOW</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
